3ee737cb241cf3bc6ec9ffbdf78df13871201110
Deepak <singh017deepak@gmail.com>
2025-02-19 16:55:50 +0530

    [Deepak] Add. Code of validate email using schema

---
+++ Week-5Day2/src/main/java/com/ioprogramming/jsondata/handonpracticeproblems/filter/Filter.java
@@ -0,0 +1,28 @@
+package com.ioprogramming.jsondata.handonpracticeproblems.filter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Filter {
+public static void filter(String path,int age) throws IOException {
+    ObjectMapper object= new ObjectMapper();
+    JsonNode jsonNodes=object.readTree(new File(path));
+    if (!jsonNodes.isArray()) {
+        System.out.println("Error: JSON file does not contain an array.");
+        return;
+    }
+    for (JsonNode jsonNode : jsonNodes){
+    if(jsonNode.get("age").asInt() > age) {
+        System.out.println("id :- " + jsonNode.get("id").asText());
+        System.out.println("name :- " + jsonNode.get("name").asText());
+        System.out.println("age :- " + jsonNode.get("age").asInt());
+        System.out.println("city :- " + jsonNode.get("city").asText());
+        System.out.println("+------------------------------------------+");
+    }
+
+    }
+}
+}
